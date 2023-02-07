8a311148d53df88b337a68c7557702163753195a
aroesec <aroe3sec@gmail.com>
2023-02-06 20:12:19 -0800

    week 1 down

---
+++ app/lib/app-stack.ts
@@ -0,0 +1,12 @@
+import { Stack, StackProps, aws_s3 as s3 } from "aws-cdk-lib"
+import { S3 } from "aws-cdk-lib/aws-ses-actions"
+import { Construct } from "constructs"
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DefaultAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const assetsStackBucket = new s3.Bucket(this, "AppAssetBucket", {})
+  }
+}
